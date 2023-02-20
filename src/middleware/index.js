94d5e0fdfb6f35107609269647c7e4e2ed361c39
Manas <shuklamanasofficial@gmail.com>
2023-02-20 14:12:25 +0530

    Initiale Commit

---
+++ src/middleware/index.js
@@ -0,0 +1,84 @@
+const { expressjwt: jwt } = require("express-jwt");
+const { UNAUTHORIZED, NOT_FOUND } = require('../utils/statusCode')
+const { loggerUtil } = require('./../utils/logger')
+const dotenv = require('dotenv')
+const User = require("../models/userModel")
+
+dotenv.config()
+
+const isSignedIn = jwt({
+    secret: process.env.SECRET,
+    algorithms: ['HS256', 'RS256'],
+    userProperty: 'auth'
+})
+
+const isValidToken = (err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        return res
+            .status(UNAUTHORIZED)
+            .json({ error: 'Authentication Failed!' })
+    }
+    next()
+}
+
+const isAuthenticated = (
+    req,
+    res,
+    next,
+) => {
+    const checker = req.profile && req.auth && req.profile.id == req.auth.id
+    if (!checker) {
+        return res.status(SC.FORBIDDEN).json({
+            error: 'ACCESS DENIED!'
+        })
+    }
+    return next()
+}
+
+const isAdmin = async (req, res, next) => {
+    const authId = req.auth._id
+
+    if (authId) {
+        User.findById(authId).exec((err, user) => {
+            if (err || !user) {
+                return res.status(NOT_FOUND).json({
+                    error: 'No user was found in DB!'
+                })
+            }
+            if (user.role === 3) {
+                next()
+            } else {
+                res.status(UNAUTHORIZED).json({
+                    error: 'Not an admin!'
+                })
+            }
+        })
+    }
+}
+
+const isSameUserOrAdmin = async (req, res, next) => {
+    console.log(req);
+    const authId = req.auth._id
+    const userId = req.params.userId
+    if (authId) {
+        User.findById(authId).exec((err, user) => {
+            if (err || !user) {
+                return res.status(NOT_FOUND).json({
+                    status: NOT_FOUND,
+                    error: 'No User Found!'
+                })
+            }
+            if (user.role === 3 || user.userId == userId) {
+                next()
+            } else {
+                res.status(UNAUTHORIZED).json({
+                    status: UNAUTHORIZED,
+                    error: 'Not an admin or Same as Logged in User'
+                })
+            }
+        })
+    }
+}
+
+
+module.exports = { isSignedIn, isValidToken, isAuthenticated, isAdmin, isSameUserOrAdmin }
---
+++ src/routes/auth.js
@@ -0,0 +1,85 @@
+// import { forgotPassword } from './../controllers/auth'
+const express = require('express')
+const { check, body } = require('express-validator')
+const { isSignedIn, isValidToken, isSameUserOrAdmin } = require('./../middleware/index')
+const { sendOtpRequest, signUp, login, signout, forgotPassword, changePassword } = require("../controllers/auth")
+
+const authRoute = express.Router()
+
+authRoute.post(
+    '/sendOtp',
+    [check('email').isEmail().withMessage('Please provide a valid E-Mail!')],
+    sendOtpRequest
+)
+
+authRoute.post(
+    '/signup',
+    [
+        check('email').isEmail().withMessage('Please provide a valid E-Mail!'),
+        check('password')
+            .isLength({ min: 8 })
+            .withMessage('Password length should be minimum of 8 characters'),
+        check('otp')
+            .isLength({ min: 4 })
+            .withMessage('OTP Length should be 4 digits')
+    ],
+    signUp
+)
+
+authRoute.post(
+    '/login',
+    [
+        check('email').isEmail().withMessage('Please provide a valid E-Mail!'),
+        check('password')
+            .isLength({ min: 8 })
+            .withMessage('Password length should be minimum of 8 characters')
+    ],
+    login
+)
+
+authRoute.get('/signout', signout)
+
+authRoute.post('/forgot-password',
+    [
+        check('newPassword')
+            .isLength({ min: 8 })
+            .withMessage('New Password length should be minimum of 8 characters'),
+        check('email').isEmail().withMessage('Please provide a valid E-Mail!'),
+        check('otp')
+            .isLength({ min: 4 })
+            .withMessage('OTP Length should be 4 digits')
+    ],
+    body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.newPassword) {
+            throw new Error('Password confirmation does not match password');
+        }
+        // Indicates the success of this synchronous custom validator
+        return true;
+    }),
+    forgotPassword
+)
+
+authRoute.post('/change-password/:userId',
+    isSignedIn,
+    isValidToken,
+    isSameUserOrAdmin,
+    [
+        check('newPassword')
+            .isLength({ min: 8 })
+            .withMessage('New Password length should be minimum of 8 characters'),
+        check('oldPassword')
+            .isLength({ min: 8 })
+            .withMessage('Old Password length should be minimum of 8 characters'),
+
+    ],
+    body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.newPassword) {
+            throw new Error('Password confirmation does not match password');
+        }
+        // Indicates the success of this synchronous custom validator
+        return true;
+    }),
+    changePassword
+)
+
+module.exports = authRoute
